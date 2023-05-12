af4ec7384a73fc6d49c8314a1be6a541e1c4e01b
gintoki <hoangquan13042001@gmail.com>
2023-05-12 08:53:39 +0700

    update 120523

--- src/main/java/com/quanht/utils/Constant.java
+++ src/main/java/com/quanht/utils/Constant.java
@@ -4,6 +4,6 @@ public class Constant {
 
     public static final int MAX_AGE_COOKIE = 7 * 24 * 60 * 60; // 1 tuần
 
-    public static final String EMAIL_FROM = "hoangquan13042001@gmail.com";
+    public static final String EMAIL_FROM = "quanht1304@gmail.com";
 
 }
